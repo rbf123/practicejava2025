3710270888716923666aab57f27552a856b4f665
Romina <108244092+rbf123@users.noreply.github.com.>
2025-07-03 19:34:41 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main (String[] args){
+        System.out.println("I lived lives, tried to play both sides");
+        System.out.println("But I couldn't find my own place");
+        System.out.println("Called a problem child 'cause I got too wild");
+        System.out.println("But that's how I'm getting paid");
+        System.out.println("on stage");
+        System.out.println("I'm done hidin now I'm shining")
+
+
+    }
+
+}
+
