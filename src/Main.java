0d8047f0f642617e8a4dc7bbf85b2e28ad8f4473
Romina <108244092+rbf123@users.noreply.github.com.>
2025-07-22 15:25:35 -0500

    practice array of objects

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = new Car("Corvette", "Blue");
+        Car car3 = new Car("Charger", "Yellow");
 
-        // Object = An entity that holds data (attributes) and can perform actions (methods), reference data type
-        // constructor = A special method to initialize objects, you can pass arguments to a constructor and set up initial value
-        // overloaded constructors = Allow a class to have multiple constructors with different parameter lists,
-        //                            enable objects to be initialized in various ways
+        Car[] cars = {car1,car2,car3}; // or Car[] = new Car[3]
 
-        User user1 = new User("Spongebob");
-        User user2 = new User("Patrick", "pstar@aol.com");
-        User user3 = new User("Sandy", "scheeks@gmail.com", 27);
-        User user4 = new User();
+        for (Car car : cars) {
+            car.color = "black";
+        }
+
+        for (Car car : cars) {
+            car.drive();
+        }
 
 
-        System.out.println(user1.username);
-        System.out.println(user1.email);
-        System.out.println(user1.age);
+        // for(int i = 0; i < cars.length; i++){
+           // cars[i].drive();}
 
-        System.out.println(user2.username);
-        System.out.println(user2.email);
-        System.out.println(user2.age);
 
-        System.out.println(user3.username);
-        System.out.println(user3.email);
-        System.out.println(user3.age);
 
-        System.out.println(user4.username);
-        System.out.println(user4.email);
-        System.out.println(user4.age);
 
     }
 
